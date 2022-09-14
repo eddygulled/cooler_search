994110b00559cf548d36630fb35696577d876968
eddygulled <edgargulay69@gmail.com>
2022-09-14 22:15:48 +0300

    deploy ready

--- search/views.py
+++ search/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import os
+import os, json
 from freezer.settings import BASE_DIR
 
 import csv
@@ -17,5 +17,24 @@ def search_code(code):
 
 # Create your views here.
 def search_view(request):
-    data = search_code(5037630)
-    return render(request, "index.html", {'data': data})
+    if request.POST:
+        code_post = request.POST['cooler_no']
+        data = search_code(code_post)
+        context = {
+            'loaded': True,
+            'outlet_no': data['OUTLET NO'],
+            'outlet_name': data['CURRENT OUTLET NAME'],
+            'outlet_location': data['CURRENT LOCATION'],
+            'mobile_number': data['CURRENT MOBILE\TEL NO'],
+            'description_model': data['Description MODEL ID'],
+            'cooler_tag': data['COOLER TAG'],
+            'region': data['REGION'],
+            'rad_name': data['RAD NAME'],
+            'asm_name': data['ASM NAME'],
+            'occd_training_name': data['OCCD TRADING NAME'],
+            'last_scanned': data['LAST SCANNED'],
+            'route_name': data['ROUTE NAME']
+        }
+        return render(request, "index.html", context)
+
+    return render(request, "index.html")
