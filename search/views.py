099dc1753b01a2e269a2f45f1ea2fdcfa63d940c
eddygulled <edgargulay69@gmail.com>
2022-09-14 21:32:20 +0300

    ready to deploy

--- search/views.py
+++ search/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+import os
+from freezer.settings import BASE_DIR
+
+import csv
+
+def search_code(code):
+    file_path = os.path.join(BASE_DIR, 'search/coolerdata.csv')
+    file = open(file_path)
+    with file as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['OUTLET NO'] == str(code):
+                return row
+        
+    return None
 
 # Create your views here.
 def search_view(request):
-    return render(request, "index.html")
+    data = search_code(5037630)
+    return render(request, "index.html", {'data': data})
--- test/test.py
+++ test/test.py
@@ -1,12 +1,14 @@
 import csv
 
+def search_code(code):
+    file = open("coolerdata.csv")
+    with file as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['OUTLET NO'] == str(code):
+                return row
+        
+    return None
 
-file = open("test/user.csv")
-
-mobile = "salum Rajabu"
-
-with file as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if row['name'] == mobile:
-            print(row)
+data = search_code(5047333)
+print(data)
