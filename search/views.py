06b40c1a99b04ca66e002c674189401475819f2a
eddygulled <edgargulay69@gmail.com>
2022-09-15 07:43:10 +0300

    map loading with hit miss message display error added

--- search/views.py
+++ search/views.py
@@ -6,7 +6,7 @@ from freezer.settings import BASE_DIR
 import csv
 
 def search_code(code):
-    file_path = os.path.join(BASE_DIR, 'search/coolerdata.csv')
+    file_path = os.path.join(BASE_DIR, 'search/coolerdata_goe.csv')
     file = open(file_path, 'r', encoding='utf-8', errors='ignore')
     with file as csvfile:
         reader = csv.DictReader(csvfile)
@@ -31,22 +31,38 @@ def record_data(request, tag, long, lat):
 def search_view(request):
     if request.POST:
         code_post = request.POST['cooler_no']
-        data = search_code(code_post)
-        context = {
-            'loaded': True,
-            'outlet_no': data['OUTLET NO'],
-            'outlet_name': data['CURRENT OUTLET NAME'],
-            'outlet_location': data['CURRENT LOCATION'],
-            'mobile_number': data['CURRENT MOBILE\TEL NO'],
-            'description_model': data['Description MODEL ID'],
-            'cooler_tag': data['COOLER TAG'],
-            'region': data['REGION'],
-            'rad_name': data['RAD NAME'],
-            'asm_name': data['ASM NAME'],
-            'occd_training_name': data['OCCD TRADING NAME'],
-            'last_scanned': data['LAST SCANNED'],
-            'route_name': data['ROUTE NAME']
-        }
-        return render(request, "index.html", context)
+        
+        # default map display
+        map=False
+        try:
+            data = search_code(code_post)
+            if data['GPS Lat'] == str(0) or data['GPS Lon'] == str(0):
+                map = False
+            else:
+                map = True
+            if   data is not None:
+                context = {
+                    'loaded': True,
+                    'map': map,
+                    'outlet_no': data['OUTLET NO'],
+                    'outlet_name': data['CURRENT OUTLET NAME'],
+                    'outlet_location': data['CURRENT LOCATION'],
+                    'mobile_number': data['CURRENT MOBILE\TEL NO'],
+                    'description_model': data['Description MODEL ID'],
+                    'cooler_tag': data['COOLER TAG'],
+                    'region': data['REGION'],
+                    'rad_name': data['RAD NAME'],
+                    'asm_name': data['ASM NAME'],
+                    'occd_training_name': data['OCCD TRADING NAME'],
+                    'last_scanned': data['LAST SCANNED'],
+                    'route_name': data['ROUTE NAME'],
+                    'lat': data['GPS Lat'],
+                    'lon': data['GPS Lon']
+                }
+                return render(request, "index.html", context)
+            else:
+                return render(request, "index.html", {'msg': 'nothing Found'}) 
+        except:
+            return render(request, "index.html", {'msg': 'nothing Found'})
 
     return render(request, "index.html")
