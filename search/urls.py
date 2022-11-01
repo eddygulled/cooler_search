24570d75ae2aab08c0b1eafbedc95ee0acf1db48
eddygulled <edgargulay69@gmail.com>
2022-11-01 19:57:30 +0300

    re_url config

--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,7 @@ from .views import home, search_view, cooler_verification,rad_cooler_verificatio
 
 urlpatterns = [
     path('', home),
+    path('activate/<int:file_id>/', activate_file, name='activate'),
     path('ici/', search_view, name='ici'),
     path('cvm/', cooler_verification_blank, name='cvm'),
     path('cvm/<int:time_jump>/<str:center>', cooler_verification),
@@ -10,6 +11,4 @@ urlpatterns = [
     path('upload/', upload_file, name='upload'),
     path('error/', error, name='error'),
     path('login/', login_view, name='login'),
-    path('activate/<int:file_id>/', activate_file, name='activate'),
-    
 ]
