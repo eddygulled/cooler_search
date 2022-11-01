df34b161f251805707262ea40930af60480466ac
eddygulled <edgargulay69@gmail.com>
2022-11-01 19:10:06 +0300

    Auto Activate

--- search/models.py
+++ search/models.py
@@ -7,7 +7,7 @@ class MasterDataFile(models.Model):
     file_name = models.CharField(max_length=100, verbose_name='Uploaded By')
     file = models.FileField()
     file_date = models.DateTimeField(auto_now=True)
-    active  = models.BooleanField(default=False)
+    active  = models.BooleanField(default=True)
 
     def __str__(self):
         return self.filename()
