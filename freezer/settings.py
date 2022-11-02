b181ea0bef67f91b8cb1317333ab256fc7e77582
eddygulled <edgargulay69@gmail.com>
2022-11-02 08:44:17 +0300

    debug True

--- freezer/settings.py
+++ freezer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*p_rqr$awbx!0y6jv#7zat7&&bm&ol@f)qk1!dzoag%4e(rst$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'eddygulled.pythonanywhere.com',
